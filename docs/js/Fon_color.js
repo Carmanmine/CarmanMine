4795fed69b9fa317f0ea1682df076071c421e413
Carmanmine <shylkin.vlad0905@gmail.com>
2024-11-21 10:41:54 +0200

    Add files via upload

---
+++ docs/js/Fon_color.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+
+document.getElementById('Button_fon_1').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+})
---
+++ docs/js/Redirect_to_programm.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+
+
+
+function redirectToSiteProgramm() {
+    window.location.href = "Developer_Programm_NOTCLICK.html";
+}
